7476a0a58e5ee3310f5ac147c3caae7ca3c5a573
HungVu <vuhung.vtq@gmail.com>
2024-12-01 19:01:01 +0700

    datn-02

--- server/helpers/cloudinary.js
+++ server/helpers/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require("cloudinary").v2;
 const multer = require("multer");
 
 cloudinary.config({
-  cloud_name: "",
-  api_key: "",
-  api_secret: "",
+  cloud_name: "doxwydhbs",
+  api_key: "872751555567377",
+  api_secret: "j6ZzpgSB78oRlAKdYn1l9u3IomE",
 });
 
 const storage = new multer.memoryStorage();
